55028a10cbad6347da1fac2e72219f6ce323fa6d
Brian Diaz <bidiaz101@gmail.com>
2021-08-13 15:36:39 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-
+function superbowlWin(record) {
+    const winYearAndResult = record.find(function(obj){
+        return obj.result === "W"
+    });
+    if (winYearAndResult === undefined) {
+        return undefined
+    } else {
+        return winYearAndResult.year;
+    }
+};
